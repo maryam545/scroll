0ae719e48ff7250a62b4f6cd2b824b261df207f9
maryam <marychch19@gmail.com>
2024-10-16 16:45:09 +0330

    end codewars

---
+++ codewars/code1.js
@@ -0,0 +1,20 @@
+//1
+// function getAge(inputString){
+//     return parseInt(inputString);
+//   }
+/**
+ *
+ *
+ *
+ */
+//2
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+//   }
+/***
+ *
+ *
+ *
+ */
+//3
+const invert = (array) => array.map((num) => -num);
